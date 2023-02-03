0dc371bac7fa0bdd70ac9d8af2a6e44d18a443d7
andrenormanlang <andrenormanlang@gmail.com>
2023-02-03 13:30:22 +0100

    added length to order_items validation

--- src/validations/order_rules.ts
+++ src/validations/order_rules.ts
@@ -4,6 +4,7 @@ export const createOrderRules = [
     body('customer_email').isEmail().withMessage('This is an e-mail field'), 
     body('customer_postcode').isString().isLength({ max: 6 }).withMessage('This postcode is a max. of 6 characters'),
     body('order_total').isInt({ min: 1 }).withMessage('Your order should contain at least 1 product'),
+    body('order_items').isLength({ min: 1 }).bail().withMessage("your basket is empty"),
     body('order_items.*.product_id').isInt({ min: 1 }).bail().withMessage("product not found"),
     body('order_items.*.qty').isInt({ min: 1 }).withMessage('The product in items should be at least 1'),
     body('order_items.*.item_price').isInt({ min: 1 }).withMessage('The price of your product should be at least 1'),
