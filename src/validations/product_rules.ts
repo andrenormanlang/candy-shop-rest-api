fc37a5986eca1994599f3fde8335b58c3cc03ad2
andrenormanlang <andrenormanlang@gmail.com>
2023-03-09 21:02:57 +0100

    new validations

--- src/validations/product_rules.ts
+++ src/validations/product_rules.ts
@@ -1,10 +1,10 @@
 import { body } from "express-validator";
 
 export const createProductRules = [
-  body('name').isString().exists().withMessage('Please provide a valid name'),
-  body('description').isString().exists().withMessage('Please provide a valid description'),
+  body('name').isString().notEmpty().withMessage('Please provide a valid name'),
+  body('description').isString().notEmpty().withMessage('Please provide a valid description'),
   body('images').isJSON().isObject().exists().withMessage('Please provide valid image'),
-  body('stock_status').isString().exists().withMessage('Please provide a valid stock status'),
+  body('stock_status').isString().notEmpty().withMessage('Please provide a valid stock status'),
 	body('price').isInt({ min: 1}).withMessage('has to be at least 1'),
   body('stock_quantity').isInt({ min: 0}).withMessage('has to be at least 0'),
 ]
