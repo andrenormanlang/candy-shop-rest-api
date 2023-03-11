388c66ae4adc4c1dcd5045f1b5d59308cb5cae32
andrenormanlang <andrenormanlang@gmail.com>
2023-03-11 14:41:41 +0100

    added extra validations for order and product

--- src/validations/order_rules.ts
+++ src/validations/order_rules.ts
@@ -1,14 +1,14 @@
 import { body } from "express-validator";
 
 export const createOrderRules = [
-    body('customer_first_name').isString().notEmpty().withMessage('Please provide a valid first name'),
-    body('customer_last_name').isString().notEmpty().withMessage('Please provide a valid last name'),
-    body('customer_address').isString().notEmpty().withMessage('Please provide a valid address'),
+    body('customer_first_name').isString().notEmpty().withMessage('Please provide a valid first name').bail().isLength({ min: 2 }).withMessage("your first name is too short"),
+    body('customer_last_name').isString().notEmpty().withMessage('Please provide a valid last name').bail().isLength({ min: 2 }).withMessage("your last name is too short"),
+    body('customer_address').isString().notEmpty().withMessage('Please provide a valid address').bail().isLength({ min: 5 }).withMessage("your address is too short"),
     body('customer_city').isString().notEmpty().withMessage('Please provide a valid city'),
     body('customer_email').isEmail().withMessage('This is an e-mail field'), 
     body('customer_postcode').isString().isLength({ max: 6 }).withMessage('This postcode is a max. of 6 characters'),
     body('order_total').isInt({ min: 1 }).withMessage('Your order should contain at least 1 product'),
-    body('order_items').isLength({ min: 1 }).bail().withMessage("your basket is empty"),
+    body('order_items').notEmpty().bail().withMessage("your basket is empty"),
     body('order_items.*.product_id').isInt({ min: 1 }).bail().withMessage("product not found"),
     body('order_items.*.qty').isInt({ min: 1 }).withMessage('The product in items should be at least 1'),
     body('order_items.*.item_price').isInt({ min: 1 }).withMessage('The price of your product should be at least 1'),
--- src/validations/product_rules.ts
+++ src/validations/product_rules.ts
@@ -1,10 +1,10 @@
 import { body } from "express-validator";
 
 export const createProductRules = [
-  body('name').isString().notEmpty().withMessage('Please provide a valid name'),
-  body('description').isString().notEmpty().withMessage('Please provide a valid description'),
+  body('name').isString().notEmpty().withMessage('Please provide a valid product name').bail().isLength({ min: 2 }).withMessage("your product name is too short"),
+  body('description').isString().notEmpty().withMessage('Please provide a valid product description').isLength({ min: 5 }).withMessage("your product description is too short"),
   body('images').notEmpty().withMessage('Please provide valid images'),
   body('stock_status').isString().notEmpty().withMessage('Please provide a valid stock status'),
-	body('price').isInt({ min: 1}).withMessage('has to be at least 1'),
-  body('stock_quantity').isInt({ min: 0}).withMessage('has to be at least 0'),
+	body('price').isInt({ min: 1}).withMessage('price should be at least 1kr'),
+  body('stock_quantity').optional({ nullable: true }).isInt({ min: 0}).withMessage('Stock quantity has to be at least 0'),
 ]
