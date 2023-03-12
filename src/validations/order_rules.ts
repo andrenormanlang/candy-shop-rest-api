f1e47bd3dc3b146af5315f8eabde3a765c67bbeb
andrenormanlang <andrenormanlang@gmail.com>
2023-03-12 11:52:09 +0100

    new validations

--- src/validations/order_rules.ts
+++ src/validations/order_rules.ts
@@ -10,7 +10,7 @@ export const createOrderRules = [
     body('order_total').isInt({ min: 1 }).withMessage('Your order should contain at least 1 product'),
     body('order_items').isInt({ min: 1 }).bail().withMessage("our basket is empty please order at least 1 item"),
     body('order_items.*.product_id').isInt({ min: 1 }).bail().withMessage("product not found"),
-    body('order_items.*.qty').isInt({ min: 1 }).withMessage('The product in items should be at least 1'),
+    body('order_items.*.qty').isInt({ min: 1 }).withMessage('Product out of stock'),
     body('order_items.*.item_price').isInt({ min: 1 }).withMessage('The price of your product should be at least 1'),
     body('order_items.*.item_total').isInt({ min: 1 }).withMessage('The total of products in items should be at least 1'),
   ]
