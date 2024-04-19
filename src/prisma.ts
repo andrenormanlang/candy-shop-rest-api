ec826ce6b01ce6f9751bc41d522f2a26b40afe9a
André Norman Lang <andrenormanlang@gmail.com>
2024-04-19 22:24:54 +0200

    connecting back to original settings

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,23 +1,6 @@
-// import { PrismaClient } from '@prisma/client'
-// const prisma = new PrismaClient({
-// 	log: ['error', 'info', 'warn']
-// })
-
-// export default prisma
-const { PrismaClient } = require("@prisma/client");
-
+import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient({
 	log: ['error', 'info', 'warn']
-});
+})
 
-(async () => {
-  try {
-    console.log(await prisma.widget.create({ data: { } }));
-  } catch (err) {
-    console.error("error executing query:", err);
-  } finally {
-    prisma.$disconnect();
-  }
-})();
-
-export default prisma;
+export default prisma
