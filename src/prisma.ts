e9d424831932ff43350921e7b9696025f180be3d
André Norman Lang <andrenormanlang@gmail.com>
2024-04-19 21:47:08 +0200

    connecting with cockroach db

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,6 +1,23 @@
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
+// const prisma = new PrismaClient({
+// 	log: ['error', 'info', 'warn']
+// })
+
+// export default prisma
+const { PrismaClient } = require("@prisma/client");
+
 const prisma = new PrismaClient({
 	log: ['error', 'info', 'warn']
-})
+});
 
-export default prisma
+(async () => {
+  try {
+    console.log(await prisma.widget.create({ data: { } }));
+  } catch (err) {
+    console.error("error executing query:", err);
+  } finally {
+    prisma.$disconnect();
+  }
+})();
+
+export default prisma;
